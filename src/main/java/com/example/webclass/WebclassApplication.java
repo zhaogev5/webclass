98fc0df539b2857a14cde2c339e0baf08b67e205
lyz <lyz.com>
2022-04-16 23:16:10 +0800

    第一次提交

---
+++ src/main/java/com/example/webclass/WebclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webclass/WebclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
